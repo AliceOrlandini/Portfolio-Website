2b658188c4724f190e5d27186035b9976d5ec0bd
AliceOrlandini <a.orlandini10@studenti.unipi.it>
2025-05-02 12:28:09 +0200

    Velite setup

--- portfolio/app/blog/page.tsx
+++ portfolio/app/blog/page.tsx
@@ -1,35 +1,33 @@
 import ArticleCard from "@/components/ArticleCard";
+import { posts } from "#site/content";
+import { sortBlogPosts } from "@/lib/utils";
 
-export default function PricingPage() {
+export default async function BlogPage() {
+  const sortedPosts = sortBlogPosts(posts.filter((post) => post.published));
+
   return (
     <section className="mx-10 my-10 flex flex-col gap-y-10">
       <h1 className="font-raleway text-2xl desktop:text-5xl font-bold leading-8 desktop:leading-14">Ti va di leggere qualche articolo?</h1>
-      <div className="grid grid-cols-12 gap-4 w-full">
-        <ArticleCard 
-          title="Article 1"
-          description="This is the description for article 1"
-          href="/blog/1"
-          image="https://images.unsplash.com/photo-1744144501170-adec17d0c893?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="col-span-3"/>
-        <ArticleCard 
-          title="Article 2"
-          description="This is the description for article 2"
-          href="/blog/2"
-          image="https://images.unsplash.com/photo-1728650388218-c10708026ad3?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="col-span-3"/>
-        <ArticleCard 
-          title="Article 3"
-          description="This is the description for article 3"
-          href="/blog/3"
-          image="https://images.unsplash.com/photo-1742218797084-3a201597b019?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="col-span-3"/>
-        <ArticleCard 
-          title="Article 4"
-          description="This is the description for article 4"
-          href="/blog/4"
-          image="https://images.unsplash.com/photo-1742199009963-c028d0c5a603?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          className="col-span-3"/>
-      </div>
+      {sortedPosts?.length > 0 ? (
+        <ul className="grid grid-cols-12 gap-4 w-full">
+          {sortedPosts.map((post) => (
+            <li key={post.slug} className="col-span-3">
+              <ArticleCard 
+                title={post.title}
+                description={post.description}
+                slug={post.slugAsParams}
+                date={post.date}
+                image="https://images.unsplash.com/photo-1744144501170-adec17d0c893?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
+            </li>
+          ))}
+
+        </ul>
+      ): (
+        <div className="col-span-12 flex justify-center items-center">
+          <h2 className="text-2xl font-semibold font-raleway">Ancora nessun articolo pubblicato...</h2>
+        </div>
+      )}
     </section>
   );
 }
--- portfolio/components/ArticleCard.tsx
+++ portfolio/components/ArticleCard.tsx
@@ -1,23 +1,26 @@
-import { cn } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
+import { Calendar } from "lucide-react"
 
 export default function ArticleCard({
   className,
   title,
   description,
-  href,
+  slug,
+  date,
   image
 }: {
   className?: string;
   title: string;
   description: string;
-  href: string;
+  slug: string;
+  date: string;
   image: string;
 }) {
   return (
-    <div className={cn("font-raleway mt-10 flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md", className)}>
+    <article className={cn("font-raleway mt-10 flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md", className)}>
       <Image 
         width={500}
         height={300}
@@ -25,11 +28,15 @@ export default function ArticleCard({
         alt="Article Image" 
         className="w-full h-48 object-cover rounded-lg -mt-10" />
       <h2 className="text-xl font-bold">{title}</h2>
+      <div className="flex items-center gap-2">
+        <Calendar size={18} />
+        <time dateTime={date}>{formatDate(date)}</time>
+      </div>
       <p className="font-medium">{description}</p>
       <div className="bg-primary mt-7 w-full justify-center flex flex-row gap-2 mx-auto text-base font-semibold items-center text-button-text py-4 px-7 rounded-2xl shadow-md hover:cursor-pointer hover:scale-105 transition-transform duration-300">
-        <Link href={href}>Leggi</Link>
+        <Link href={slug}>Leggi</Link>
         <ArrowUpRight size={20}/>
       </div>
-    </div>
+    </article>
   );
 }
